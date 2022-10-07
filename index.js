0331d79986af41d645ca28f9afc1ce7635740fe6
Prince Thind <princethind799@gmail.com>
2022-10-07 12:30:17 +0530

    implement basic lift mechanism

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import state from "./lib/state.js";
-import UI from "./lib/UI.js";
+import UI, { setupUIButtons } from "./lib/UI.js";
 import { getExactFloor, updateLiftWaitingStatus } from "./lib/utility.js";
 
 
+init();
+
+function init() {
+    setupUIButtons();
+    requestAnimationFrame(animate);
+
+}
+
 function animate() {
 
-    if (state.waiting) return requestAnimationFrame(animate);
+    if (state.waiting || state.floors.length == 0) return requestAnimationFrame(animate);
 
-    const floor=getExactFloor(state.height);
-    if(floor){
+    const floor = getExactFloor(state.height);
+    if (floor) {
         updateLiftWaitingStatus(floor)
+
     }
 
     UI.lift.style.marginTop = `${state.height}px`;
@@ -35,5 +44,4 @@ function animate() {
 }
 
 
-requestAnimationFrame(animate);
 
