b59bc629fbc4454eb43f43baf438b02a61bfccc6
Prince Thind <princethind799@gmail.com>
2022-10-07 18:11:54 +0530

    add keyboard support

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import state from "./lib/state.js";
-import UI, { setupUIButtons } from "./lib/UI.js";
+import UI, { addFloor, setupUIButtons } from "./lib/UI.js";
 import { getExactFloor, updateDirection, updateLiftStatus } from "./lib/utility.js";
 
 
@@ -8,15 +8,16 @@ init();
 function init() {
     setupUIButtons();
     requestAnimationFrame(animate);
+    window.addEventListener('keydown', addFloorViaKey)
 }
 
 function animate() {
     if (state.waiting || state.floors.length == 0) return requestAnimationFrame(animate);
-   
+
     updateDirection();
 
     const floor = getExactFloor(state.height);
-    if (floor) {
+    if (floor!=null) {
         updateLiftStatus(floor)
     }
 
@@ -28,6 +29,7 @@ function animate() {
 function moveLift() {
     UI.lift.style.marginTop = `${state.height}px`;
 
+   
     if (state.height <= 0) {
         state.direction = 'bottom'
     }
@@ -46,3 +48,9 @@ function moveLift() {
 }
 
 
+
+function addFloorViaKey(e) {
+    const pressed = +String.fromCharCode(e.keyCode);
+    if (![0,1, 2, 3, 4, 5, 6, 7, 8, 9].includes(pressed)) return;
+    addFloor(pressed)
+}
