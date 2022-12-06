07097b26bb39cc0ad45a4134986adf807f8c133d
Prince Thind <princethind799@gmail.com>
2022-12-06 18:49:07 +0530

    updates

---
+++ v1.0.0/index.js
@@ -0,0 +1,56 @@
+import state from "./lib/state.js";
+import UI, { addFloor, setupUIButtons } from "./lib/UI.js";
+import { getExactFloor, updateDirection, updateLiftStatus } from "./lib/utility.js";
+
+
+init();
+
+function init() {
+    setupUIButtons();
+    requestAnimationFrame(animate);
+    window.addEventListener('keydown', addFloorViaKey)
+}
+
+function animate() {
+    if (state.waiting || state.floors.length == 0) return requestAnimationFrame(animate);
+
+    updateDirection();
+
+    const floor = getExactFloor(state.height);
+    if (floor != null) {
+        updateLiftStatus(floor)
+    }
+
+    moveLift();
+    requestAnimationFrame(animate);
+
+}
+
+function moveLift() {
+    UI.lift.style.marginTop = `${state.height}px`;
+
+
+    if (state.height <= 0) {
+        state.direction = 'bottom'
+    }
+
+    if (state.height >= state.maxHeight) {
+        state.direction = 'top'
+    }
+
+    if (state.direction == 'bottom') {
+        state.height += state.speed;
+    }
+
+    if (state.direction == 'top') {
+        state.height -= state.speed;
+    }
+}
+
+
+
+function addFloorViaKey(e) {
+    const pressed = +String.fromCharCode(e.keyCode);
+    if (![0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(pressed)) return;
+    addFloor(pressed)
+}
---
+++ v1.0.0/lib/utility.js
@@ -0,0 +1,45 @@
+import state from "./state.js"
+import { toggleButtonActive, toggleLiftActiveStyle } from "./UI.js";
+
+function updateLiftStatus(floor) {
+
+    if (!state.floors.includes(floor)) return;
+
+    state.floor = floor;
+    state.waiting = true;
+
+
+    toggleLiftActiveStyle();
+    setTimeout(() => {
+        state.waiting = false;
+        toggleLiftActiveStyle()
+        removeFloor(floor)
+    }, 1000);
+
+}
+
+function removeFloor(floor) {
+    toggleButtonActive(floor);
+    const floorIndex = state.floors.findIndex(f => f == floor);
+    state.floors.splice(floorIndex, 1);
+}
+
+function getExactFloor(height) {
+    const decimal = height / (3 * 16);
+    if (decimal != Math.trunc(decimal)) return null;
+    return 9 - decimal
+}
+
+function updateDirection() {
+    const floor = state.floor;
+
+    if (state.direction == 'top') {
+        if (!state.floors.some(f => f > floor)) state.direction = 'bottom'
+    }
+    else {
+        if (!state.floors.some(f => f < floor)) state.direction = 'top'
+
+    }
+}
+
+export { updateLiftStatus, getExactFloor, updateDirection, removeFloor }
